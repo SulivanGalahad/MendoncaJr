e5bdefe5bca920d19516578dfd4a172bd0dad9e6
SulivanGalahad <sulivanpm@gmail.com>
2025-02-27 00:50:39 -0400

    1 - DbContext

---
+++ Login.API/DbContexts/LoginDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Login.API.Entities;
+
+namespace Login.API.DbContexts;
+
+public class LoginDbContext(DbContextOptions<LoginDbContext> options) : DbContext(options)
+{
+    public DbSet<Access> Logins { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+
+}
---
+++ Login.API/Entities/Login.cs
@@ -0,0 +1,18 @@
+﻿namespace Login.API.Entities;
+
+public class Access
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public Access()
+    {
+        
+    }
+
+    public Access(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+}
